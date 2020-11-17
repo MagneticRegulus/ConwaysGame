d9d401ff846d6a953ebe4623d7e834ebb3254dc0
MagneticRegulus <ladymeckbert@gmail.com>
2020-11-17 14:30:18 +1300

    Added methods to generate random living cells when new Game is started.

--- src/application/Game.java
+++ src/application/Game.java
@@ -1,17 +1,43 @@
 package application;
 
+import java.util.HashMap;
+import java.util.Map;
+import javafx.geometry.Point2D;
+import javafx.scene.canvas.GraphicsContext;
+
 public class Game {
 	
 	private int rows;
 	private int cols;
 	private double pix;
 	private double cellSize;
+	private Map<Point2D, Cell> cells = new HashMap<>();
 	
 	public Game(int dim, double size) {
 		this.rows = dim;
 		this.cols = dim;
 		this.pix = size;
 		this.cellSize = size / dim;
+	}
+	
+	public void generateCells(GraphicsContext graphics) {
+		double rand;
+		for (int x = 0; x < rows; x++) {
+			for (int y = 0; y < cols; y++) {
+				rand = Math.random();
+				if (rand <= 0.05) {
+					cells.put(new Point2D(x, y), new Cell());
+				}
+			}
+		}
+		draw(graphics);
+	}
+	
+	public void draw(GraphicsContext graphics) {
+		for (Point2D pt : cells.keySet()) {
+			graphics.setFill(cells.get(pt).getColor());
+			graphics.fillRect(pt.getX(), pt.getY(), 1, 1);
+		}
 	}
 	
 	public double getPix() {
