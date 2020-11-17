f41e8d37dfbf4838124b03f573f3677bd3f56060
MagneticRegulus <ladymeckbert@gmail.com>
2020-11-17 20:38:58 +1300

    Add logic to Game

--- src/application/Game.java
+++ src/application/Game.java
@@ -2,8 +2,12 @@ package application;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+
 import javafx.geometry.Point2D;
 import javafx.scene.canvas.GraphicsContext;
 
@@ -12,17 +16,17 @@ public class Game {
 	private int rows;
 	private int cols;
 	private double pix;
-	private double cellSize;
+	private double canSize;
 	private Map<Point2D, Cell> cells = new HashMap<>();
 	
 	public Game(int dim, double size) {
 		this.rows = dim;
 		this.cols = dim;
 		this.pix = size;
-		this.cellSize = size / dim;
+		this.canSize = size;
 	}
 	
-	public void generateCells(GraphicsContext graphics) {
+	public void generateCells() {
 		double rand;
 		for (int x = 0; x < rows; x++) {
 			for (int y = 0; y < cols; y++) {
@@ -32,65 +36,103 @@ public class Game {
 				}
 			}
 		}
-		draw(graphics);
+	}
+	
+	public void generateSpecial() {
+		cells.put(new Point2D(24, 23), new Cell(true));
+		cells.put(new Point2D(24, 24), new Cell(true));
+		cells.put(new Point2D(24, 25), new Cell(true));
 	}
 	
 	public void draw(GraphicsContext graphics) {
+		graphics.clearRect(0, 0, canSize, canSize);
 		for (Point2D pt : cells.keySet()) {
 			graphics.setFill(cells.get(pt).getColor());
 			graphics.fillRect(pt.getX(), pt.getY(), 1, 1);
+		}
+		
+		for (int x = 0; x <= rows; x++) {
+			graphics.strokeLine(x, 0, x, cols);
+		}
+		for (int y = 0; y <= cols; y++) {
+			graphics.strokeLine(0, y, rows, y);
 		}
 	}
 	
 	public void updateGame() {
 		addDeadNeighbours();
+		//System.out.println("Added the relevant dead");
 		prepNextRound();
+		//System.out.println("Set the next round");
 		updateCells();
-		removeDead();
+		//System.out.println("Updated the cells");
+		//removeDead();
+		//System.out.println("Removed the dead");
 	}
 	
 	public void addDeadNeighbours() {
-		List<Point2D> neighbours = new ArrayList<>();
-		for (Point2D pt : cells.keySet()) {
-			if (cells.get(pt).isAlive()) {
-				neighbours = getNeighbours(pt);
+		Set<Point2D> neighbours = new HashSet<>();
+		Set<Point2D> dead = new HashSet<>();
+		Iterator<Map.Entry<Point2D, Cell>> it = cells.entrySet().iterator();
+		while (it.hasNext()) {
+			Map.Entry<Point2D, Cell> pt = it.next();
+			if (pt.getValue().isAlive()) {
+				//System.out.println("Point " + pt.getKey().getX() + ", " + pt.getKey().getY() + " is alive.");
+				neighbours = getNeighbours(pt.getKey());
 				for (Point2D n : neighbours) {
 					if (!cells.containsKey(n)) {
-						cells.put(n, new Cell(false));
+						dead.add(n);
 					}
 				}
 			}
 		}
+		int countDead = 0;
+		for (Point2D d : dead) {
+			cells.put(d, new Cell(false));
+			countDead++;
+		}
+		//System.out.println("Added " + countDead + " dead.");
 	}
 	
 	public void prepNextRound() {
 		int count = 0;
 		for (Point2D pt : cells.keySet()) {
 			count = countLiving(getNeighbours(pt));
-			if (!cells.get(pt).isAlive() && count == 3) { cells.get(pt).setNextState(true); }
-			else if (cells.get(pt).isAlive() && (count == 2 || count == 3)) { cells.get(pt).setNextState(true); }
-			else { cells.get(pt).setNextState(false); }
+			if (!cells.get(pt).isAlive() && count == 3) { 
+				cells.get(pt).setNextState(true); 
+				//System.out.println(" is dead but will be alive.");
+			}
+			else if (cells.get(pt).isAlive() && (count == 2 || count == 3)) { 
+				cells.get(pt).setNextState(true);
+				//System.out.println(" is alive and will stay that way.");
+			}
+			else { 
+				cells.get(pt).setNextState(false);
+				//System.out.println(" will be dead.");
+			}
 		}
 	}
 	
-	public List<Point2D> getNeighbours(Point2D pt) {
-		List<Point2D> neighbours = new ArrayList<>();
+	public Set<Point2D> getNeighbours(Point2D pt) {
+		Set<Point2D> neighbours = new HashSet<>();
 		neighbours.add(new Point2D(pt.getX() - 1, pt.getY() - 1));
 		neighbours.add(new Point2D(pt.getX(), pt.getY() - 1));
 		neighbours.add(new Point2D(pt.getX() + 1, pt.getY() - 1));
-		neighbours.add(new Point2D(pt.getX(), pt.getY() - 1));
-		neighbours.add(new Point2D(pt.getX(), pt.getY() + 1));
+		neighbours.add(new Point2D(pt.getX() - 1, pt.getY()));
+		neighbours.add(new Point2D(pt.getX() + 1, pt.getY()));
 		neighbours.add(new Point2D(pt.getX() - 1, pt.getY() + 1));
 		neighbours.add(new Point2D(pt.getX(), pt.getY() + 1));
 		neighbours.add(new Point2D(pt.getX() + 1, pt.getY() + 1));
+		//System.out.print("Point " + pt.getX() + ", " + pt.getY() + " Alive: " + cells.get(pt).isAlive() + " ");
 		return neighbours;
 	}
 	
-	public int countLiving(List<Point2D> neighbours) {
+	public int countLiving(Set<Point2D> neighbours) {
 		int count = 0;
 		for (Point2D n : neighbours) {
 			if (cells.get(n) != null && cells.get(n).isAlive()) { count++; }
 		}
+		//System.out.print("count: "+ count);
 		return count;
 	}
 	
@@ -101,10 +143,10 @@ public class Game {
 	}
 	
 	public void removeDead() {
-		for (Point2D pt : cells.keySet()) {
-			if (!cells.get(pt).isAlive()) {
-				cells.remove(pt);
-			}
+		Iterator<Map.Entry<Point2D, Cell>> it = cells.entrySet().iterator();
+		while (it.hasNext()) {
+			Map.Entry<Point2D, Cell> pt = it.next();
+			if (!pt.getValue().isAlive()) { it.remove(); }
 		}
 	}
 	
@@ -118,10 +160,6 @@ public class Game {
 
 	public int getCols() {
 		return cols;
-	}
-
-	public double getCellSize() {
-		return cellSize;
 	}
 	
 }
